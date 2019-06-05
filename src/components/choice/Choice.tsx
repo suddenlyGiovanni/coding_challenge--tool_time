b6ef7a808125c374bf1f62d33747bc36a0d679ee
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2019-06-05 21:36:01 +0200

    feat(basic-styling): completed UX wireframe

--- src/components/choice/Choice.tsx
+++ src/components/choice/Choice.tsx
@@ -1,11 +1,35 @@
 import * as React from 'react'
 
+import { createStyles, makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import ListItem from '@material-ui/core/ListItem'
+
+
+const useStyles = makeStyles(
+  createStyles({
+    listItem: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      // backgroundColor: 'lightgrey',
+      marginTop: 5,
+      marginBottom: 5,
+    },
+    copyElem: {
+      minWidth: 100,
+    },
+  })
+)
+
 interface Props {
   choice: string
   votes: number
   percentage: number
   url: string
   onClick: (url: string) => void
+  selected?: boolean
 }
 
 export const Choice: React.FC<Props> = ({
@@ -13,11 +37,21 @@ export const Choice: React.FC<Props> = ({
   votes = 0,
   percentage = 0,
   url = '',
+  selected = false,
   onClick,
 }) => {
+  const classes = useStyles()
+
   return (
-    <div onClick={() => onClick(url)}>
-      <span>{choice}</span> <span>{votes}</span> <span>{percentage}</span>
-    </div>
+    <ListItem
+      className={classes.listItem}
+      button
+      selected={selected}
+      onClick={() => onClick(url)}
+    >
+      <Typography className={classes.copyElem}>{choice}</Typography>
+      <Typography className={classes.copyElem}>Votes: {votes}</Typography>
+      <Typography className={classes.copyElem}>{percentage}%</Typography>
+    </ListItem>
   )
 }
--- src/screens/questions-view/QuestionsView.tsx
+++ src/screens/questions-view/QuestionsView.tsx
@@ -3,22 +3,26 @@ import { RouteChildrenProps } from 'react-router'
 import { connect } from 'react-redux'
 import { RootState } from 'typesafe-actions'
 
+// Material UI
+import Grid from '@material-ui/core/Grid'
 import { pollsSelectors, pollsActions } from 'features/polls'
 import { Question } from 'components/question/Question'
+import { BodyWrapper } from 'components/body-wrapper/BodyWrapper'
 
 function mapStateToProps(state: RootState) {
-  return {
-    questions: pollsSelectors.getQuestions(state),
-  }
+  return { questions: pollsSelectors.getQuestions(state) }
 }
 
-const mapDispatchToProps = {
-  fetchQuestions: pollsActions.fetchQuestions,
-}
+const mapDispatchToProps = { fetchQuestions: pollsActions.fetchQuestions }
 
 type ReduxProps = ReturnType<typeof mapStateToProps> & typeof mapDispatchToProps
 
 type Props = RouteChildrenProps & ReduxProps
+
+export const Fallback: React.FC = () => {
+  // TODO: provide a better fallback
+  return <div>Loading</div>
+}
 
 export class QuestionsView extends React.Component<Props> {
   public componentDidMount() {
@@ -29,26 +33,25 @@ export class QuestionsView extends React.Component<Props> {
   public render() {
     const { questions } = this.props
 
-    // if (!questions) return null // TODO: provide a better fallback
+    if (!questions) return Fallback
 
     return (
-      <div>
-        <span>Questions</span>
-        <div>
-          {questions &&
-            questions.map(({ choices, published_at, question, url }) => {
-              return (
+      <BodyWrapper title={'Questions'}>
+        <Grid container spacing={3}>
+          {questions.map(({ choices, published_at, question, url }) => {
+            return (
+              <Grid key={url} item md={4} sm={6} xs={12}>
                 <Question
-                  key={url}
                   question={question}
                   numberOfChoices={choices.length}
                   publishedAt={published_at}
                   onClick={() => this.handleSelection(url)}
                 />
-              )
-            })}
-        </div>
-      </div>
+              </Grid>
+            )
+          })}
+        </Grid>
+      </BodyWrapper>
     )
   }
 }
